a781b3618b933b32019f9dcd24ca36836f546296
Oleksii Aleksieiev <reneguarde@gmail.com>
2025-04-24 17:27:28 +0300

    Change validation error messages in order to match field names

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(2).max(30).required().messages({
-    "string.min": "Name must be at least 2 characters long.",
-    "string.max": "Name must be no more than 30 characters long.",
-    "any.required": "Name is required.",
+    "string.min": "name must be at least 2 characters long.",
+    "string.max": "name must be no more than 30 characters long.",
+    "any.required": "name is required.",
   }),
   email: Joi.string().email().required().messages({
-    "string.email": "Email must be a valid format (e.g., name.second@sub.dom).",
-    "any.required": "Email is required.",
+    "string.email": "email must be a valid format (e.g., name.second@sub.dom).",
+    "any.required": "email is required.",
   }),
   phone: Joi.string()
     .pattern(/^[+]?[(]?[0-9]{1,4}[)]?[-\s./0-9]*$/)
@@ -17,20 +17,20 @@ export const createContactSchema = Joi.object({
     .required()
     .messages({
       "string.pattern.base":
-        "Phone number must be a valid format (e.g., +1234567890 or (123) 456-7890).",
-      "string.min": "Phone number must be at least 7 characters long.",
-      "string.max": "Phone number must be no more than 20 characters long.",
-      "any.required": "Phone number is required.",
+        "phone must be a valid format (e.g., +1234567890 or (123) 456-7890).",
+      "string.min": "phone must be at least 7 characters long.",
+      "string.max": "phone must be no more than 20 characters long.",
+      "any.required": "phone is required.",
     }),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(2).max(30).messages({
-    "string.min": "Name must be at least 2 characters long.",
-    "string.max": "Name must be no more than 30 characters long.",
+    "string.min": "name must be at least 2 characters long.",
+    "string.max": "name must be no more than 30 characters long.",
   }),
   email: Joi.string().email().messages({
-    "string.email": "Email must be a valid format (e.g., name.second@sub.dom).",
+    "string.email": "email must be a valid format (e.g., name.second@sub.dom).",
   }),
   phone: Joi.string()
     .pattern(/^[+]?[(]?[0-9]{1,4}[)]?[-\s./0-9]*$/)
@@ -38,8 +38,8 @@ export const updateContactSchema = Joi.object({
     .max(20)
     .messages({
       "string.pattern.base":
-        "Phone number must be a valid format (e.g., +1234567890 or (123) 456-7890).",
-      "string.min": "Phone number must be at least 7 characters long.",
-      "string.max": "Phone number must be no more than 20 characters long.",
+        "phone must be a valid format (e.g., +1234567890 or (123) 456-7890).",
+      "string.min": "phone must be at least 7 characters long.",
+      "string.max": "phone must be no more than 20 characters long.",
     }),
 });
