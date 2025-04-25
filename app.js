55620749e3bb7ba59c53296d8d6e8dca8b54c6e8
Oleksii Aleksieiev <reneguarde@gmail.com>
2025-04-25 12:04:03 +0300

    Add Sequelize usage, db connection and contact model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import sequelize from "./db/Sequelize.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
